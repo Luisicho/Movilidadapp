1a3eb7a4b6879bef756d3ff91e8ef41304304622
Luisicho <luism18.1999@gmail.com>
2023-06-05 08:50:44 -0700

    first commit

---
+++ movilidadapp/movilidadapp/doctype/afectado/test_afectado.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Luis Miguel Hernandez Macias 2023 and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAfectado(FrappeTestCase):
+	pass
---
+++ movilidadapp/movilidadapp/doctype/vehiculo/vehiculo.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Luis Miguel Hernandez Macias 2023 and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Vehiculo', {
+	// refresh: function(frm) {
+
+	// }
+});
